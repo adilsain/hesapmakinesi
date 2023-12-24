44177c995f540f701f00181c764d87bf8b9ca1da
Adil Sain <adilsain@hotmail.com>
2023-12-24 22:02:16 +0300

    u

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,18 @@ void main() {
 }
 
 class HesapMakinesi extends StatefulWidget {
-  const HesapMakinesi({super.key});
+  const HesapMakinesi({Key? key}) : super(key: key);
 
   @override
   State<HesapMakinesi> createState() => _HesapMakinesiState();
 }
 
 class _HesapMakinesiState extends State<HesapMakinesi> {
-//Tutucular
-
-  late int sayi1;
-  late int sayi2;
+  late double sayi1;
+  late double sayi2;
   String gosterilecekSayi = "";
   String islemGecmisi = "";
-  late String sonuc;
+  late String sonuc = "0";
   late String islemTutucu;
 
 //Button tıklama
@@ -29,10 +27,8 @@ class _HesapMakinesiState extends State<HesapMakinesi> {
     print(buttonDegeriTutucu);
 
     if (buttonDegeriTutucu == "AC") {
-      //hepsini sil
-
-      gosterilecekSayi = "";
-      sonuc = "";
+      gosterilecekSayi = "0";
+      sonuc = "0";
       sayi1 = 0;
       sayi2 = 0;
       islemGecmisi = "";
@@ -46,67 +42,74 @@ class _HesapMakinesiState extends State<HesapMakinesi> {
         sonuc = gosterilecekSayi.substring(1);
       }
     } else if (buttonDegeriTutucu == "%") {
-      sayi1 = int.parse(gosterilecekSayi);
+      sayi1 = double.parse(gosterilecekSayi);
       islemTutucu = (sayi1 / 100).toString();
       sonuc = islemTutucu;
     } else if (buttonDegeriTutucu == "÷") {
-//Gösterilen sayiyi sayi 1 'e aktar, sonucu temizle
-      sayi1 = int.parse(gosterilecekSayi);
+      sayi1 = double.parse(gosterilecekSayi);
       sonuc = "";
       islemTutucu = buttonDegeriTutucu;
     } else if (buttonDegeriTutucu == "x") {
-      //Gösterilen sayiyi sayi 1 'e aktar, sonucu temizle
-      sayi1 = int.parse(gosterilecekSayi);
+      sayi1 = double.parse(gosterilecekSayi);
       sonuc = "";
       islemTutucu = buttonDegeriTutucu;
     } else if (buttonDegeriTutucu == "-") {
-      //Gösterilen sayiyi sayi 1 'e aktar, sonucu temizle
-      sayi1 = int.parse(gosterilecekSayi);
+      sayi1 = double.parse(gosterilecekSayi);
       sonuc = "";
       islemTutucu = buttonDegeriTutucu;
     } else if (buttonDegeriTutucu == "+") {
-      //Gösterilen sayiyi sayi 1 'e aktar, sonucu temizle
-
-      sayi1 = int.parse(gosterilecekSayi);
+      sayi1 = double.parse(gosterilecekSayi);
       sonuc = "";
       islemTutucu = buttonDegeriTutucu;
     } else if (buttonDegeriTutucu == "=") {
-      sayi2 = int.parse(gosterilecekSayi);
+      sayi2 = double.parse(gosterilecekSayi);
 
       if (islemTutucu == "+") {
         //toplama yaptır
         sonuc = (sayi1 + sayi2).toString();
-        islemGecmisi =
-            sayi1.toString() + islemTutucu.toString() + sayi2.toString();
+        islemGecmisi = "$sayi1 $islemTutucu $sayi2";
       } else if (islemTutucu == "-") {
         //çıkartma yaptır
         sonuc = (sayi1 - sayi2).toString();
-        islemGecmisi =
-            sayi1.toString() + islemTutucu.toString() + sayi2.toString();
+        islemGecmisi = "$sayi1 $islemTutucu $sayi2";
       } else if (islemTutucu == "x") {
         //çarpma yaptır
         sonuc = (sayi1 * sayi2).toString();
-        islemGecmisi =
-            sayi1.toString() + islemTutucu.toString() + sayi2.toString();
+        islemGecmisi = "$sayi1 $islemTutucu $sayi2";
       } else if (islemTutucu == "÷") {
-        //bölme yaptır
-        sonuc = (sayi1 / sayi2).toString();
-        islemGecmisi =
-            sayi1.toString() + islemTutucu.toString() + sayi2.toString();
+        double bolumSonucu = sayi1 / sayi2;
+
+        // Bölme sonucu bir tam sayı ise virgül eklenmez
+        sonuc = bolumSonucu % 1 == 0
+            ? bolumSonucu.toInt().toString()
+            : bolumSonucu.toString();
+        islemGecmisi = "$sayi1 $islemTutucu $sayi2";
       }
+
+      // Eğer sonucun sonunda ".0" varsa kaldır
+      sonuc = sonuc.contains(".")
+          ? sonuc.replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")
+          : sonuc;
+      islemGecmisi = islemGecmisi.contains(".")
+          ? islemGecmisi.replaceAll(RegExp(r"(\.0)(?!\d)"), "")
+          : islemGecmisi;
     } else if (buttonDegeriTutucu == "C") {
       //islem gecmisi haric yerleri sil
       gosterilecekSayi = "";
-      sonuc = "";
+      sonuc = "0";
       sayi1 = 0;
       sayi2 = 0;
-    } else if (buttonDegeriTutucu == ",") {
-      sayi1 = int.parse(gosterilecekSayi);
-      sonuc = gosterilecekSayi + ",";
-    }
-    //rakamlara tıklandıgındaaaa
-    else {
-      sonuc = int.parse(gosterilecekSayi + buttonDegeriTutucu).toString();
+    } else if (buttonDegeriTutucu == ".") {
+      // Daha önce "." eklenmişse tekrar eklenmez
+      if (!gosterilecekSayi.contains(".")) {
+        sonuc = gosterilecekSayi + ".";
+      }
+    } else {
+      sonuc = double.parse(gosterilecekSayi + buttonDegeriTutucu).toString();
+      // Eğer sonucun sonunda ".0" varsa kaldır
+      sonuc = sonuc.contains(".")
+          ? sonuc.replaceAll(RegExp(r"([.]*0)(?!.*\d)"), "")
+          : sonuc;
     }
 
     //Durum yenileme
@@ -121,210 +124,211 @@ class _HesapMakinesiState extends State<HesapMakinesi> {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Hesap Makinesi"),
-            centerTitle: true,
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                alignment: Alignment(1.0, 1.0),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 12, 8),
-                  child: Text(
-                    islemGecmisi,
-                    style: GoogleFonts.rubik(
-                      textStyle: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                      ),
+        appBar: AppBar(
+          title: Text("Hesap Makinesi"),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              alignment: Alignment(1.0, 1.0),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 12, 8),
+                child: Text(
+                  islemGecmisi,
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-              Container(
-                alignment: Alignment(1.0, 1.0),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 0, 12, 8),
-                  child: Text(
-                    gosterilecekSayi,
-                    style: GoogleFonts.rubik(
-                      textStyle: TextStyle(
-                        fontSize: 40,
-                        color: Colors.white,
-                      ),
+            ),
+            Container(
+              alignment: Alignment(1.0, 1.0),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 12, 8),
+                child: Text(
+                  gosterilecekSayi,
+                  style: GoogleFonts.rubik(
+                    textStyle: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  HesapMakinesiButtonu(
-                    metin: "AC",
-                    dolguRengi: 0xFFB5B5B5,
-                    metinRengi: 0xff000000,
-                    metinBoyutu: 24.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "+/-",
-                    dolguRengi: 0xFFB5B5B5,
-                    metinRengi: 0xff000000,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "%",
-                    dolguRengi: 0xFFB5B5B5,
-                    metinRengi: 0xff000000,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "÷",
-                    dolguRengi: 0xFFFF9626,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 50.0,
-                    tiklama: btnTiklama,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  HesapMakinesiButtonu(
-                    metin: "7",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 24.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "8",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "9",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "x",
-                    dolguRengi: 0xFFFF9626,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 40.0,
-                    tiklama: btnTiklama,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  HesapMakinesiButtonu(
-                    metin: "4",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 24.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "5",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "6",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "-",
-                    dolguRengi: 0xFFFF9626,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 50.0,
-                    tiklama: btnTiklama,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  HesapMakinesiButtonu(
-                    metin: "1",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 24.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "2",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "3",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "+",
-                    dolguRengi: 0xFFFF9626,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 44.0,
-                    tiklama: btnTiklama,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  HesapMakinesiButtonu(
-                    metin: "0",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 24.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: ",",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "C",
-                    dolguRengi: 0xFF646363,
-                    metinRengi: 0xff000000,
-                    metinBoyutu: 30.0,
-                    tiklama: btnTiklama,
-                  ),
-                  HesapMakinesiButtonu(
-                    metin: "=",
-                    dolguRengi: 0xFFFF9626,
-                    metinRengi: 0xFFFFFFFF,
-                    metinBoyutu: 44.0,
-                    tiklama: btnTiklama,
-                  ),
-                ],
-              ),
-            ],
-          )),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HesapMakinesiButtonu(
+                  metin: "AC",
+                  dolguRengi: 0xFFB5B5B5,
+                  metinRengi: 0xff000000,
+                  metinBoyutu: 24.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "+/-",
+                  dolguRengi: 0xFFB5B5B5,
+                  metinRengi: 0xff000000,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "%",
+                  dolguRengi: 0xFFB5B5B5,
+                  metinRengi: 0xff000000,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "÷",
+                  dolguRengi: 0xFFFF9626,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 50.0,
+                  tiklama: btnTiklama,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HesapMakinesiButtonu(
+                  metin: "7",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 24.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "8",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "9",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "x",
+                  dolguRengi: 0xFFFF9626,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 40.0,
+                  tiklama: btnTiklama,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HesapMakinesiButtonu(
+                  metin: "4",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 24.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "5",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "6",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "-",
+                  dolguRengi: 0xFFFF9626,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 50.0,
+                  tiklama: btnTiklama,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HesapMakinesiButtonu(
+                  metin: "1",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 24.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "2",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "3",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "+",
+                  dolguRengi: 0xFFFF9626,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 44.0,
+                  tiklama: btnTiklama,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                HesapMakinesiButtonu(
+                  metin: "0",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 24.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: ".",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "C",
+                  dolguRengi: 0xFF646363,
+                  metinRengi: 0xff000000,
+                  metinBoyutu: 30.0,
+                  tiklama: btnTiklama,
+                ),
+                HesapMakinesiButtonu(
+                  metin: "=",
+                  dolguRengi: 0xFFFF9626,
+                  metinRengi: 0xFFFFFFFF,
+                  metinBoyutu: 44.0,
+                  tiklama: btnTiklama,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
